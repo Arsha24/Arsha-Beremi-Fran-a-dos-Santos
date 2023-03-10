aa4f697ddedd4cf33601d7a0bec3644d5c197410
Arsha24 <126976185+Arsha24@users.noreply.github.com>
2023-03-10 19:52:23 -0300

    Update tabela.php

--- tabela.php
+++ tabela.php
@@ -2,9 +2,24 @@
 
 function select_vilao() {
 	global $conn;
-	$sth = $conn->prepare("SELECT * FROM anime");
+	$sth = $conn->prepare("SELECT * FROM vilao");
 	$sth->execute();
 	return $sth->fetchAll();
 }
-
+function select_protagonista() {
+	global $conn;
+	$sth = $conn->prepare("SELECT * FROM protagonista");
+	$sth->execute();
+	return $sth->fetchAll();
+}
+function insert_protagonista($id_prota, $nome, $prota) {
+	global $conn;
+	$sth = $conn->prepare("insert into protagonista ( id_nota, nota, opiniao) values ('$id_nota', '$nota', $opiniao)");
+	$sth->execute();
+}
+function excluir_protagonista($id_prota) {
+	global $conn;
+	$sth = $conn->prepare("delete from  where id_prota = $id_prota");
+	$sth->execute();
+}
 ?>
