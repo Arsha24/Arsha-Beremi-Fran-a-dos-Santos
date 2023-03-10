bd4c67884bbd45ab7a4944662e718e618a679ac4
Arsha24 <126976185+Arsha24@users.noreply.github.com>
2023-03-10 19:50:11 -0300

    Update excluir_opiniao.php

--- excluir_opiniao.php
+++ excluir_opiniao.php
@@ -2,8 +2,8 @@
 require_once("banco.php");
 require_once("tabelas.php");
 
-$id = $_GET['id_vilao'];
-excluir_movel($id);
+$id = $_GET['id_prota'];
+excluir_protagonista($id);
 
 header("Location: sua_opiniao.php");
 
